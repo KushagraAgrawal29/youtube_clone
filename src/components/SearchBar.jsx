34284fd2acb176993cfee54606561f4238bc5d66
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-01-18 16:06:59 +0530

    Created feed searchbar and sidebar components

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import { Search } from "@mui/icons-material";
+import { IconButton, Paper } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const SearchBar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
 
-export default SearchBar
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    // Check if string is empty or contains whitespaces
+    const isEmptyOrSpaces = (str) => {
+      return /^\s*$/.test(str);
+    };
+
+    if (!isEmptyOrSpaces(searchTerm)) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
+
+  return (
+    <Paper
+      component="form"
+      onSubmit={handleOnSubmit}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      {/* Search Bar  */}
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        title="Search"
+      />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search/>
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,49 @@
+import { Stack } from "@mui/material";
+import React from "react";
+import { categories } from "../utils/constants";
+
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => {
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {/* categories  */}
+      {categories.map((category) => (
+        <button
+          className="category-btn"
+          style={{
+            background: category.name === selectedCategory && "#FC1503",
+            color: "#fff",
+          }}
+          onClick={() => setSelectedCategory(category.name)}
+          key={category.name}
+        >
+          {/* Category Icon */}
+          <span
+            style={{
+              color: category.name === selectedCategory ? "#fff" : "red",
+              marginRight: "15px",
+            }}
+          >
+            {category.icon}
+          </span>
+          {/* Category Name */}
+          <span
+            style={{
+              opacity: category.name === selectedCategory ? "1" : "0.8",
+            }}
+          >
+            {category.name}
+          </span>
+        </button>
+      ))}
+    </Stack>
+  );
+};
+
+export default Sidebar;
