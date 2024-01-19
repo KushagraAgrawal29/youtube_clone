38843059846723fd857a2fd6fe1d2b58a22375bc
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-01-19 14:07:54 +0530

    created video card and channel card and finalised project

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { Box, Typography } from '@mui/material'
+import React, { useState,useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Videos } from "./";
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const SearchFeed = () => {
+  const [videos,setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  // fetch videos from api
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+  
   return (
-    <div>
-      Search Feed
-    </div>
+    <Box  p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      {/* Title */}
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "#fff" }}>
+        Search Results for:{" "}
+        <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+      </Typography>
+
+      {/* Videos */}
+      <Videos videos={videos} />
+    </Box>
   )
 }
 
